7c28efb733eacb6595878aec9accc28aa43ce633
Sr Kanna <kannas@flip2.engr.oregonstate.edu>
2016-05-21 15:56:02 -0700

    bootstrap

---
+++ html/signup.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="../css/bootstrap.min.css" rel="stylesheet">		<!--Link stylesheet from bootstrap-->
+	<link href="../css/custom.css" rel="stylesheet">	
+<title>SR Kanna</title>
+</head>
+<body>
+
+<div class="jumbotron">
+<p id="register">Already Registered? Please Login in.</p>
+<button type="button" class="btn btn-default navbar-btn">Sign Up</button>
+<button type="button" class="btn btn-default navbar-btn">Sign In</button>
+<button type="button" class="btn btn-default navbar-btn">Search</button>
+
+
+
+
+  <h2>Sign up for Telly</h2>
+
+<form class="form-horizontal">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Remember me
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Sign Up</button>
+    </div>
+  </div>
+</form>
+
+
+</div>
+
+</body>
+
+</html>
