db00370684f3e25fcf26a9c16c28cef574fc8a60
Brandon <leebran@onid.oregonstate.edu>
2016-05-26 15:15:31 -0700

    Progress on homepage with implementing video uploading

--- views/homepage.php
+++ views/homepage.php
@@ -21,12 +21,80 @@ session_start();
     <link rel="shortcut icon" href="../images/Telly.ico">
 </head>
 <body>
-    <p>
-        homepage
-    </p>
-    
-    <?php
-        print_r($_SESSION);
-    ?>
+    <!-- --------------------------------- The two main wrappers for the site --------------------------------- -->
+    <div class="site-wrapper">
+        <div class="site-wrapper-inner">
+    <!-- --------------------------------- Navigation Bar --------------------------------- -->
+          <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+              <div class="container">
+                  <div class="navbar-header">
+                      <a class="navbar-brand" href="#">Telly</a>
+                  </div>
+                  <ul class="nav navbar-nav navbar-right">
+                      <li><a href="#"><?php echo $_SESSION['username']; ?></a></li>
+                  </ul>
+                  <form data-toggle="validator" role="form" class="navbar-form pull-right" action="submit_logout.php" method="post">
+                      <button type="submit" class="btn">Logout</button>
+                  </form>
+              </div>
+          </div>
+        <!----------------------End nav bar and begin main page, left column----------------- -->
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-6">
+                      <h3><?php echo "Welcome " . $_SESSION['username'] . "!" ?></h3>
+
+                      <h4>Upload a video here:</h4>
+                      <form data-toggle="validator" role="form" class="form-inline" action="upload_video.php" autocomplete="off" method="post">
+                          <input class="span2" name="videoname" type="text" placeholder="Video Title" required>
+                          <input class="span2" name="videolink" type="text" placeholder="Youtube Video URL" required>
+                          <button type="submit" class="btn btn-primary">Upload</button>
+                      </form>
+
+                      <p>Here are some of your videos:</p>
+
+                      <!-- <?php print_r($_SESSION); ?> -->
+
+                      <!-- Profile picture, update later -->
+                      <img src="images/default_profile_photo.png">
+                      <br><br>
+                      <!-- User's Name -->
+                      <h3> <?php echo $_SESSION['firstname'] . " " . $_SESSION['lastname']; ?> </h3>
+                      <br>
+                      <!-- User's username -->
+                      <h4><?php echo "Username: " . $_SESSION['username']; ?></h4>
+                      <br>
+                      <!-- User's email -->
+                      <h4><?php echo "Email: " . $_SESSION['email']; ?></h4>
+                  </div>
+                  <!-----------------------------Begin Right column --------------------------- -->
+                  <div class="col-md-6">
+                      <!-- User's profile, name, and current status -->
+                      <img src="images/default_profile_photo.png" width='50' height ='50'>
+                      <?php echo $_SESSION['firstname'] . " " . $_SESSION['lastname']; ?>
+                      <h3><?php echo 'Status: '; echo $_SESSION['status']; ?></h3>
+                      <br>
+                      <!-- Form to update status -->
+                      <form class="form-inline" role="form" autocomplete="off" action="update_status.php" method="post">
+                          <div class="form-group">
+                              <input name="status" type="text" class="form-control" placeholder="What's on your mind?">
+                              <button type="submit" class="btn btn-primary">Update</button>
+                          </div>
+                      </form>
+                      <iframe width="560" height="315" src="https://www.youtube.com/embed/DDyT-Nynl8w" frameborder="0" allowfullscreen></iframe>
+                  </div>
+              </div>
+              <!-- --------------------------------- Footer --------------------------------- -->
+              <div class="mastfoot">
+                  <div class="inner">
+                      <p>(C) Brandon Lee & SR Kanna 2016</p>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </div>
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- Validation Library -->
+    <script src="js/validator.js"></script>
 </body>
 </html>
